fadbd65fe40f26eba4448f78968ab09e3e1a5035
Troels Henriksen <athas@sigkill.dk>
2021-01-17 21:15:40 +0100

    Oops, missed this.

---
+++ literate.mk
@@ -0,0 +1,14 @@
+## General rules for compiling all literate Futhark programs to Markdown.
+
+FUTHARK_BACKEND ?= multicore
+
+all: $(patsubst %.fut, %.md, $(wildcard *.fut))
+
+%.md: %.fut
+	rm -rf $*-img
+	futhark literate --backend=$(FUTHARK_BACKEND) --stop-on-error $<
+
+.PHONY: clean
+
+clean:
+	rm -f $(patsubst %.fut, %.md, $(wildcard *.fut))
